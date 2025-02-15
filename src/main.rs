8d030241525b698ba8244a33e53ff02e71a668e9
Robin TONCOURT <robin.toncourt@gmail.com>
2025-02-16 00:38:03 +0100

    Writing an OS in Rust - Philipp Oppermann - A Minimal Rust Kernel

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,26 @@
 
 use core::panic::PanicInfo;
 
-#[no_mangle] // Don't mangle the name of this funciton.
-pub extern "C" fn _start() -> ! {
-    // This function is the entry point, since the linker looks for a function
-    // named `_start` by default.
-    loop {}
-}
-
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+static HELLO: &[u8] = b"Hello World!";
+
+// This function is the entry point, since the linker looks for a function
+// named `_start` by default.
+#[no_mangle] // Don't mangle the name of this funciton.
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
