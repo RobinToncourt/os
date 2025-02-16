645b391157678124c81f5de8142503a0a0f203ea
Robin TONCOURT <robin.toncourt@gmail.com>
2025-02-16 11:31:45 +0100

    Writing an OS in Rust - Philipp Oppermann - VGA Text Mode

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,25 @@
 #![no_std]  // Don't link the Rust standart library.
 #![no_main] // Disable all Rust-level entry points.
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{info}");
     loop {}
 }
-
-static HELLO: &[u8] = b"Hello World!";
 
 // This function is the entry point, since the linker looks for a function
 // named `_start` by default.
 #[no_mangle] // Don't mangle the name of this funciton.
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    let world = "World";
+    println!("Hello {world}{}", "!");
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    panic!("Panic test.");
 
     loop {}
 }
