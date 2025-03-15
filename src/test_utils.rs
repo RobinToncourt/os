74ff93721030cd1a83b2af18252bbcaffdad1650
Robin TONCOURT <robin.toncourt@gmail.com>
2025-03-15 19:59:51 +0100

    Correcting the asserts mistake.

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -10,8 +10,6 @@ use crate::stack_string::StackString;
 use crate::{exit_qemu, QemuExitCode};
 use crate::{serial_print, serial_println};
 
-const ASSERTION_IS_NOT_UTF8: &str = "Assertion message is not UTF-8.";
-
 lazy_static! {
     pub static ref TEST_STATE: Mutex<TestState> = Mutex::new(TestState::default());
 }
@@ -36,11 +34,7 @@ where
         self();
         if TEST_STATE.lock().failed {
             serial_println!("{}", Red("FAILED"));
-            serial_println!(
-                "{}",
-                core::str::from_utf8(TEST_STATE.lock().error_message.get_buffer())
-                    .unwrap_or(ASSERTION_IS_NOT_UTF8)
-            );
+            serial_println!("{}", TEST_STATE.lock().error_message);
             TEST_STATE.lock().clear();
         } else {
             serial_println!("{}", Green("ok"));
@@ -58,8 +52,8 @@ pub fn test_runner(tests: &[&dyn Testable]) {
 
 #[derive(Default)]
 pub struct TestState {
-    failed: bool,
-    error_message: StackString<{ u8::MAX as usize }>,
+    pub failed: bool,
+    pub error_message: StackString<{ u8::MAX as usize }>,
 }
 
 impl TestState {
@@ -70,7 +64,7 @@ impl TestState {
 
 impl fmt::Write for TestState {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        self.error_message.push_str(s);
+        let _ = self.error_message.push_str(s);
         Ok(())
     }
 }
@@ -80,7 +74,7 @@ macro_rules! assert_eq {
     ($left:expr, $right:expr $(,)?) => {
         if !($left == $right) {
             use core::fmt::Write;
-            let test_state: &mut TestState = &mut TEST_STATE.lock();
+            let test_state: &mut $crate::test_utils::TestState = &mut $crate::test_utils::TEST_STATE.lock();
             test_state.failed = true;
             let _ = test_state.write_fmt(format_args!(
                 " left = {:#?}\nright = {:#?}\n",
@@ -97,7 +91,7 @@ macro_rules! assert {
     ($cond:expr $(,)?) => {
         if !$cond {
             use core::fmt::Write;
-            let test_state: &mut TestState = &mut TEST_STATE.lock();
+            let test_state: &mut $crate::test_utils::TestState = &mut $crate::test_utils::TEST_STATE.lock();
             test_state.failed = true;
             let _ = test_state.write_fmt(format_args!("{}", "Assertion failed."));
             return;
@@ -123,8 +117,6 @@ fn panic(info: &PanicInfo) -> ! {
 
 #[cfg(test)]
 mod assert_tests {
-    use super::*;
-
     #[test_case]
     fn failing_assert_test() {
         assert!(false);
