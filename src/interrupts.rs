107eb2138edc8b5388f06c4bdf63b8df2279c9aa
Robin TONCOURT <robin.toncourt@gmail.com>
2025-05-31 19:55:02 +0200

    Writing an OS in Rust - Philipp Oppermann - Introduction to Paging

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -3,11 +3,12 @@ use pc_keyboard::{layouts, HandleControl, Keyboard, ScancodeSet1};
 use pic8259::ChainedPics;
 use spin;
 use x86_64::instructions::port::Port;
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 
 use crate::coquille::COQUILLE;
 use crate::gdt;
 use crate::println;
+use crate::hlt_loop;
 
 pub const PIC_1_OFFSET: u8 = 32;
 pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
@@ -33,6 +34,7 @@ lazy_static! {
         }
         idt[InterruptIndex::Timer.into_usize()].set_handler_fn(timer_interrupt_handler);
         idt[InterruptIndex::Keyboard.into_usize()].set_handler_fn(keyboard_interrupt_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
         idt
     };
 
@@ -98,6 +100,19 @@ extern "x86-interrupt" fn double_fault_handler(
     _error_code: u64,
 ) -> ! {
     panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    hlt_loop();
 }
 
 #[cfg(test)]
